b09ccb13ba0fcf34bbd9ebff1392f1da67bcfb73
CLIFORDBUHURU <148441223+CLIFORDBUHURU@users.noreply.github.com>
2026-09-13 12:23:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed= new Typed(".text", {
+    strings:["Software Developer,","Web Developer,","Data Analyst,","ICT Support Specialist,","Network Technician,"],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+});
